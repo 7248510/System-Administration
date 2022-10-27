c9179b7c8a966bd2527cb750fa1f01d6300126a5
Caleb <56181475+7248510@users.noreply.github.com>
2022-10-26 21:36:56 -0400

    Create remove-gitlab.sh

---
+++ remove-gitlab.sh
@@ -0,0 +1,14 @@
+gitlabDelete() {
+    sudo gitlab-ctl uninstall
+    sudo gitlab-ctl cleanse
+    sudo killall -u git  # This is the gitlab user account
+    sudo dpkg -P gitlab-ce #Remove the gitlab package
+    sudo rm -rf /opt/gitlab/
+    sudo apt autoremove
+    sudo rm -rf /etc/apt/sources.list.d
+    systemctl | grep gitlab # = This will check if Gitlab still has a service
+    systemctl disable gitlab-runsvdir #Disable the service
+    systemctl daemon-reload #Reload the daemon
+    systemctl reset-failed #systemctl reset-failed
+    systemctl | grep gitlab # Check if the service is still there. It shouldn't be
+}
